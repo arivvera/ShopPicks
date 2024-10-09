cdf6033d814769fa3c7712576398bc2d411335da
laynierdev <laynierdev@gmail.com>
2024-10-09 16:14:05 -0400

    #27 assets of component moved to app/assets/ and adapted component to new route css of the page is imported as module

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,56 +1,54 @@
 import React from 'react';
 import '../globals.css';
-import './faq.css';
-
-
+import styles from './page.module.css';
 const FaqPage: React.FC = () => {
     return (
-        <div className={"faq-page"}>
+        <div className={styles["faq-page"]}>
             <h1 className={"title-gradient"}>Frequently Asked Questions</h1>
             <section className="questions-section">
-                <div className="questions boxes-text-description">
+                <div className={`${styles["questions"]} boxes-text-description`}>
                     <ul>
-                        <article className={"list-answer-pair"}>
-                        <li className={"list-question"}>What is your return policy?</li>
-                        <li className={"list-answer"}>
+                        <article className={styles["list-answer-pair"]}>
+                        <li className={styles["list-question"]}>What is your return policy?</li>
+                        <p className={styles[styles["list-answer"]]}>
                             We accept returns within 30 days of purchase. Items must be in their original condition
                                 and packaging. Please contact our customer service for a return authorization.
-                        </li>
+                        </p>
                         </article>
-                        <article className={"list-answer-pair"}>
-                        <li className={"list-question"}>How do I track my order?</li>
-                        <li className={"list-answer"}>
+                        <article className={styles["list-answer-pair"]}>
+                        <li className={styles["list-question"]}>How do I track my order?</li>
+                        <p className={styles["list-answer"]}>
                             Once your order has shipped, you will receive an email with a tracking number.
                             You can use this number on our website to track the status of your orders .
-                        </li>
+                        </p>
                         </article>
                         <article className={"list-answer-pair"}>
-                        <li className={"list-question"}>Do you offer free shipping?</li>
-                        <li className={"list-answer"}>
+                        <li className={styles["list-question"]}>Do you offer free shipping?</li>
+                        <p className={styles["list-answer"]}>
                             Yes, we offer free shipping on orders over $50.
                             For orders under that amount, a flat shipping fee will apply.
-                        </li>
+                        </p>
                         </article>
                         <article className={"list-answer-pair"}>
-                        <li className={"list-question"}>How do you protect my privacy?</li>
-                        <li className={"list-answer"}>We take your privacy seriously.
+                        <li className={styles["list-question"]}>How do you protect my privacy?</li>
+                        <p className={styles["list-answer"]}>We take your privacy seriously.
                             All personal information is encrypted and stored securely.
                             We do not share your information with third parties without your consent.
-                        </li>
+                        </p>
                         </article>
                         <article className={"list-answer-pair"}>
-                        <li className={"list-question"}>Can I change or cancel my order?</li>
-                        <li className={"list-answer"}>If you need to change or cancel your order,
+                        <li className={styles["list-question"]}>Can I change or cancel my order?</li>
+                        <p className={styles["list-answer"]}>If you need to change or cancel your order,
                             please contact us within 24 hours of placing your order. Once the order has been processed,
                             we may not be able to make changes.
-                        </li>
+                        </p>
                         </article>
                         <article className={"list-answer-pair"}>
-                        <li className={"list-question"}>What payment methods do you accept?</li>
-                        <li className={"list-answer"}>
+                        <li className={styles["list-question"]}>What payment methods do you accept?</li>
+                        <p className={styles["list-answer"]}>
                             We accept all major credit cards, PayPal, and Apple Pay.
                             You can choose your preferred payment method at checkout.
-                        </li>
+                        </p>
                         </article>
 
                     </ul>
